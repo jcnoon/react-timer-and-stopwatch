55d859ba172c71dd3439d5957b316607400ff549
Jacob <jacob.ca.noonan@gmail.com>
2022-09-18 23:16:51 -0700

    fix jsdoc comments

--- src/interfaces/ITimerBase.ts
+++ src/interfaces/ITimerBase.ts
@@ -4,7 +4,7 @@ import { Timestamps, DirectionOfTime, Time } from "../types/types";
 export interface ITimerBase {
     timestamps: Timestamps;
     intervalRate: number;
-    //** Whether timer should continue after finished in negative numbers */
+    /** Whether timer should continue after finished in negative numbers */
     continueAfterFinish: boolean;
     directionOfTime: DirectionOfTime;
     /** Get the milliseconds which will be used with Display class */
@@ -17,6 +17,6 @@ export interface ITimerBase {
     IsPastFinish: () => boolean;
     /** Time elapsed in timer in milliseconds */
     timeElapsed: number;
-    //** Time left until timer is finished in milliseconds. For Stopwatch, this is always 0. */
+    /** Time left until timer is finished in milliseconds. For Stopwatch, this is always 0. */
     timeLeft: number;
 }
